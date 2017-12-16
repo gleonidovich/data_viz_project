3e3c03a879013c1bc8e4037494fa5e5c8b57acdd
gleonidovich <grigory.makarenko@gmail.com>
2017-12-16 12:32:24 -0500

    basic scatterplot with nesting

--- js/main.js
+++ js/main.js
@@ -1,31 +1,50 @@
-var xCanvas = 700;
-var yCanvas = 400;
-var padding = 100;
+var margin = 50;
+var width = 950 - margin;
+var height = 500 - margin;
 
-var dataset = d3.csv("data/cancellation_count_per_year.csv", function(d) {
-  return {
-    year: new Date(+d.Year, 0, 1),
-    cancellations: d.Cancelled_Count
-  };
-}, function(error, rows) {
-  console.log(rows);
-});
-
-var xScale = d3.time.scale()
-                    .domain(d3.extent(dataset, function(d) {return d.year;}))
-                    .range([padding, xCanvas - padding * 2]);
-
-var yScale = d3.scale.linear()
-                     .domain(d3.extent(dataset, function(d) {return d.cancellations;}))
-                     .range([yCanvas - padding, padding]);
+var parseTime = d3.timeParse("%Y %m %d");
 
 var svg = d3.select("body")
             .append("svg")
-            .attr("width", xCanvas)
-            .attr("height", yCanvas);
+            .attr("width", width + margin)
+            .attr("height", height + margin);
 
-svg.selectAll("rect")
-   .data(dataset)
-   .enter().append("rect")
-   .attr("x", function(d) {return xScale(d.year);})
-   .attr("y", function(d) {return yScale(d.cancellations);});
+var draw_points = function(data) {
+  var nestedData = d3.nest()
+    .key(function(d) {return new Date(d.date.getUTCFullYear(), d.date.getMonth());})
+    .rollup(function(v) {return d3.sum(v, function(d) {return d.cancel;});})
+    .entries(data);
+
+  console.log(nestedData)
+
+  var xScale = d3.scaleTime()
+                 .domain(d3.extent(nestedData, function(d) {return d.key;}))
+                 .range([0 + margin, width]);
+
+  var yScale = d3.scaleLinear()
+                 .domain([0, d3.max(nestedData, function(d) {return d.value;})])
+                 .range([height, 0]);
+
+  svg.selectAll("circle")
+     .data(nestedData)
+     .enter()
+     .append("circle")
+     .attr("cx", function(d) {return xScale(d.key);})
+     .attr("cy", function(d) {return yScale(d.value);})
+     .attr("r", 2);
+
+  svg.append("g")
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(xScale));
+
+  svg.append("g")
+     .attr("transform", "translate(" + margin + ",0)")
+     .call(d3.axisLeft(yScale));
+};
+
+d3.csv("data/flight_data.csv", function(d) {
+  return {
+    date: parseTime(d.Year + " " + d.Month + " " + d.DayofMonth),
+    cancel: +d.Cancelled
+  };
+}, draw_points);
