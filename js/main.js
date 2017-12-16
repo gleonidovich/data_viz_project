a072d3e56ca53629220afaecb54cc411e3abe65a
gleonidovich <grigory.makarenko@gmail.com>
2017-12-16 18:05:54 -0500

    added transition and updating

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-var margin = 50;
-var width = 950 - margin;
-var height = 500 - margin;
-
 var parseTime = d3.timeParse("%Y %m %d");
 
-var svg = d3.select("body")
-            .append("svg")
-            .attr("width", width + margin)
-            .attr("height", height + margin);
+function draw(data) {
+  var margin = 50;
+  var width = 950 - margin;
+  var height = 500 - margin;
 
-var draw = function(data) {
+  function convertKeytoDate(d) {
+    for (var i = 0; i < d.length; i++) {
+      d[i].key = new Date(d[i].key);
+    }
+  };
+
   var dataMonth = d3.nest()
     .key(function(d) {
       return parseTime(d.key.getUTCFullYear() + " " + d.key.getMonth() + " 1");
@@ -20,7 +21,6 @@ var draw = function(data) {
       });
     })
     .entries(data);
-
   convertKeytoDate(dataMonth);
 
   var dataYear = d3.nest()
@@ -33,27 +33,32 @@ var draw = function(data) {
       });
     })
     .entries(dataMonth);
-
   convertKeytoDate(dataYear)
 
-  function convertKeytoDate(d) {
-    for (var i = 0; i < d.length; i++) {
-      d[i].key = new Date(d[i].key);
-    }
+  function createXScale(data) {
+    return d3.scaleTime()
+             .domain(d3.extent(data, function(d) {
+               return new Date(d.key);
+             }))
+             .range([margin, width]);
   };
 
-  function drawPoints(data) {
-    var xScale = d3.scaleTime()
-                   .domain(d3.extent(data, function(d) {
-                      return new Date(d.key);
-                    }))
-                   .range([margin, width]);
+  function createYScale(data) {
+    return d3.scaleLinear()
+             .domain([0, d3.max(data, function(d) {
+               return d.value;
+             })])
+             .range([height, margin]);
+  };
 
-    var yScale = d3.scaleLinear()
-                   .domain([0, d3.max(data, function(d) {
-                      return d.value;
-                    })])
-                   .range([height, margin]);
+  function init(data) {
+    var xScale = createXScale(data);
+    var yScale = createYScale(data);
+
+    var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width + margin)
+            .attr("height", height + margin);
 
     svg.selectAll("circle")
        .data(data)
@@ -62,24 +67,79 @@ var draw = function(data) {
        .attr("cx", function(d) {
           return xScale(d.key);
         })
-       .attr("cy", function(d) {return yScale(d.value);})
+       .attr("cy", function(d) {
+          return yScale(d.value);
+       })
        .attr("r", 2);
 
     svg.append("g")
+       .attr("class", "xAxis")
        .attr("transform", "translate(0," + height + ")")
        .call(d3.axisBottom(xScale));
 
     svg.append("g")
+       .attr("class", "yAxis")
        .attr("transform", "translate(" + margin + ",0)")
        .call(d3.axisLeft(yScale));
   };
 
-  drawPoints(dataYear);
+  function update(data) {
+    var svg = d3.select("svg");
+
+    var xScale = createXScale(data);
+    var yScale = createYScale(data);
+
+    var circles = svg.selectAll("circle")
+      .data(data, function(d) {
+        return d.key;
+      });
+
+    var xAxis = svg.selectAll(".xAxis");
+    var yAxis = svg.selectAll(".yAxis");
+
+    circles.attr("class", "update")
+           .transition()
+           .duration(1000)
+           .attr("cy", function(d) {
+              return yScale(d.value);
+            });
+
+    circles.exit()
+           .attr("class", "exit")
+           .transition()
+           .duration(1000)
+           .attr("r", 0)
+           .remove();
+
+    circles.enter()
+           .append("circle")
+           .attr("class", "enter")
+           .attr("cx", function(d) {
+              return xScale(d.key);
+           })
+           .attr("cy", function(d) {
+              return yScale(d.value);
+           });
+
+    xAxis.transition()
+         .duration(1000)
+         .call(d3.axisBottom(xScale));
+
+    yAxis.transition()
+         .duration(1000)
+         .call(d3.axisLeft(yScale));
+  };
+
+  init(data);
+
+  window.setTimeout(function() {update(dataMonth);}, 3000);
+  window.setTimeout(function() {update(dataYear);}, 6000);
 };
 
-d3.csv("data/flight_data.csv", function(d) {
-  return {
-    key: parseTime(d.Year + " " + d.Month + " " + d.DayofMonth),
-    value: +d.Cancelled
-  };
-}, draw);
+d3.csv("data/flight_data.csv", function(data) {
+  data.forEach(function(d) {
+    d.key = parseTime(d.Year + " " + d.Month + " " + d.DayofMonth);
+    d.value = +d.Cancelled;
+    });
+  draw(data);
+});
